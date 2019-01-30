be6ff5b652fc661fe785dc39cd248e49336b914d
Iwan Kruger <iwankruger@gmail.com>
2019-01-30 12:09:35 +0200

    Added controllers

---
+++ src/controllers/user.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+// define the home page route
+router.get('/', (req, res) => {
+    res.send('Users home page');
+});
+
+// define the about route
+router.get('/about', (req, res) => {
+    res.send('About users');
+});
+
+export = router;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 const app = express();
+import user from './controllers/user';
+
+//  Connect all our routes to our application
+app.use('/users', user);
 
 app.get('/', (req, res) => {
-    res.send('Hello world!!!')
+    res.send('Hello world!!!');
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
