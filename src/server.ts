4c7af67a9f8ade940c6eb532ef663567e1394df5
Iwan Kruger <iwankruger@gmail.com>
2019-02-21 23:17:03 +0200

    Added swagger-ui-express

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,16 @@ import express from 'express';
 const app = express();
 import user from './controllers/user';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 //  Connect all our routes to our application
 app.use('/users', user);
 
 app.get('/', (req, res) => {
     res.send('Hello world!!!');
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
