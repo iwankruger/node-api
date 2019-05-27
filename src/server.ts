f8aeb65941d121f6c337dd60d8152ba5ab6f3462
Iwan Kruger <iwankruger@gmail.com>
2019-05-28 01:26:57 +0200

    Added basic auth

--- src/server.ts
+++ src/server.ts
@@ -3,54 +3,13 @@ import swaggerUi from 'swagger-ui-express';
 import user from './controllers/user';
 import swaggerSpec from './swaggerConfiguration';
 import passport from 'passport';
-import {Strategy as LocalStrategy } from 'passport-local';
 import * as bodyParser from 'body-parser';
 import * as verify from './verify';
 
 const app = express();
 
-/*function auth(req, res, next) {
-    console.log(req.header);
-    const authHeader = req.headers.authorization;
-
-    console.log(JSON.stringify(authHeader));
-
-    const error = new Error('You are not authenticated!');
-    error['status'] = 401;
-
-    if (!authHeader) {
-        return next(error);
-    }
-
-    const authData = new Buffer (authHeader.split(' ')[1], 'base64').toString().split(':');
-    const username = authData[0];
-    const password = authData[1];
-
-    if (username === 'admin' && password === 'password') {
-        return next();
-    }
-
-    return next(error);
-
-
-}*/
-
-
-//app.use(auth);
-
-
 app.use(bodyParser.json());
-
 app.use(passport.initialize());
-/*passport.use(new LocalStrategy((username, password, next) => {
-    console.log('debug local strategy ');
-    if (username === 'admin' && password === 'password') {
-        return next(null, {name: 'name'});
-    }
-
-    return next(null, false);
-}));*/
-
 
 app.use((error, req, res, next) => {
    res.writeHead(error.status || 500, {
@@ -62,16 +21,11 @@ app.use((error, req, res, next) => {
 });
 
 
-
-
 //  Connect all our routes to our application
 app.use('/users', user);
-
 app.post('/test', verify.verifyOrdinaryUser, (req, res) => {
     res.send('Hello world10!!!');
 });
-
-console.log('hello world10!!!');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
--- src/verify.ts
+++ src/verify.ts
@@ -1,18 +1,35 @@
 import passport from 'passport';
-import {Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { BasicStrategy } from 'passport-http'
 
 passport.use(new LocalStrategy((username, password, next) => {
     console.log('debug local strategy ');
+    // todo: get verify username and password against database
     if (username === 'admin' && password === 'password') {
-        return next(null, {name: 'name'});
+        return next(null, { name: 'name' });
     }
 
     return next(null, false);
+
 }));
 
-export let verifyOrdinaryUser = passport.authenticate('local', { session: false });
+passport.use(new BasicStrategy((username, password, next) => {
+    console.log('debug basic strategy ');
+    // todo: get verify username and password against database
+    if (username === 'admin' && password === 'password') {
+        return next(null, { name: 'name' });
+    }
+
+    return next(null, false);
+
+}));
+
+export let verifyOrdinaryUserLocalStrategy = passport.authenticate('local', { session: false });
+export let verifyOrdinaryUser = passport.authenticate('basic', { session: false });
 
 /*
+todo: remove notes
+
 export function verifyOrdinaryUser(req, res, next) {
     console.log('verify ordinary user');
     let error: any = new Error('You are not authenticated!');
@@ -25,6 +42,32 @@ export function verifyAdmin(req, res, next) {
     console.log('verify admin');
     return next();
 }
+
+function auth(req, res, next) {
+    console.log(req.header);
+    const authHeader = req.headers.authorization;
+
+    console.log(JSON.stringify(authHeader));
+
+    const error = new Error('You are not authenticated!');
+    error['status'] = 401;
+
+    if (!authHeader) {
+        return next(error);
+    }
+
+    const authData = new Buffer (authHeader.split(' ')[1], 'base64').toString().split(':');
+    const username = authData[0];
+    const password = authData[1];
+
+    if (username === 'admin' && password === 'password') {
+        return next();
+    }
+
+    return next(error);
+
+
+}
+app.use(auth);
+
 */
-
-
