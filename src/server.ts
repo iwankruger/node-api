9ef8ab6e5e43fa1155c9f3b38b42d95b10c4276f
Iwan Kruger <iwankruger@gmail.com>
2019-05-28 14:24:41 +0200

    Added authentication for the swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -22,14 +22,14 @@ app.use((error, req: Request, res: Response, next: NextFunction) => {
 
 
 //  Connect all our routes to our application
-app.use('/users', user);
+app.use('/api/users', user);
 
 // todo: this is just for illustration purposes, remove example
 app.post('/test', verify.verifyOrdinaryUserBasic, (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello world10!!!');
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', verify.verifyOrdinaryUserBasic, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
